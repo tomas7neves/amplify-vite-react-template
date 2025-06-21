ee3ffc1fabd42bc9479a31a0e1e25065557703c6
Tomás Neves <tmb.neves@campus.fct.unl.pt>
2025-06-22 00:08:37 +0100

    added per-user data isolation

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,5 +7,28 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    phone: true,
+  },
+  userAttributes: {
+    "custom:display_name": {
+      dataType: "String",
+      mutable: true,
+      maxLen: 16,
+      minLen: 1,
+    },
+    "custom:favorite_number": {
+      dataType: "Number",
+      mutable: true,
+      min: 1,
+      max: 100,
+    },
+    "custom:is_beta_user": {
+      dataType: "Boolean",
+      mutable: true,
+    },
+    "custom:started_free_trial": {
+      dataType: "DateTime",
+      mutable: true,
+    },
   },
 });
